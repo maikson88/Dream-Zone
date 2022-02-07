5f415e74db87added8883ecdde02eae8b1bac93a
MAIKSON MENDES CARDOSO DA SILVA <maiksonmendes1988@gmail.com>
2022-02-06 22:20:02 -0300

    CheckGround by dotproduct and CheckIfThereIsSlopes
    those methods are necessary to improve the character interaction with the environment

--- Dream Zone/Assets/Scripts/Core/CoreComponents/CollisionSenses.cs
+++ Dream Zone/Assets/Scripts/Core/CoreComponents/CollisionSenses.cs
@@ -23,17 +23,17 @@ public class CollisionSenses : MonoBehaviour
     [SerializeField]
     private float groundCheckRadius;
 
+
+
     public bool StepCheck()
     {
         RaycastHit hitLower;
         if (Physics.Raycast(smallStepLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, rayDistance))
         {
-            Debug.Log("Detected Lower 1");
             RaycastHit hitUpper;
             if (!Physics.Raycast(smallStepUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, rayDistance))
             {
-                Debug.Log("Not Detected upper 1");
-
+                //Debug.Log("Detected1");
                 return true;
             }
         }
@@ -41,11 +41,10 @@ public class CollisionSenses : MonoBehaviour
         RaycastHit hitLower45;
         if (Physics.Raycast(smallStepLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, rayDistance))
         {
-            Debug.Log("Detected Lower 2");
             RaycastHit hitUpper45;
             if (!Physics.Raycast(smallStepUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, rayDistance))
             {
-                Debug.Log("Detected Upper 2");
+                //Debug.Log("Detected2");
                 return true;
             }
         }
@@ -53,16 +52,69 @@ public class CollisionSenses : MonoBehaviour
         RaycastHit hitLowerMinus45;
         if (Physics.Raycast(smallStepLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, rayDistance))
         {
-            Debug.Log("Detected Lower 3");
             RaycastHit hitUpperMinus45;
             if (!Physics.Raycast(smallStepUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, rayDistance))
             {
-                Debug.Log("Detected Upper 3");
+                //Debug.Log("Detected3");
                 return true;
             }
         }
 
         return false;
+    }
+
+    public Vector3 GetGroundNormal()
+    {
+        RaycastHit hitFo;
+        Vector3 cachedNormal;
+
+
+        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hitFo, rayDistance))
+        return Vector3.up;
+
+        cachedNormal = hitFo.normal;
+
+        //Converting angle to dot
+        float minGroundAngle = 40;
+        float angleToRadians = minGroundAngle * Mathf.Deg2Rad;
+        float dotGround = Mathf.Cos(angleToRadians);
+
+        //Comparing if raycast is considered Ground
+        if (CheckIfTouchingGround() && cachedNormal.y >= dotGround)
+            return cachedNormal;
+        else return Vector3.up;
+    }
+
+    public bool CheckIfTheresSlopeNear()
+    {
+        RaycastHit hitFo;
+        Vector3 cachedNormal;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hitFo, rayDistance * 2f))
+            if(CheckIsSlope()) return true;
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(1.5f, 0, 1), out hitFo, rayDistance * 2f))
+            if(CheckIsSlope()) return true;
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitFo, rayDistance * 2f))
+            if(CheckIsSlope()) return true;
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hitFo, rayDistance * 2f))
+            if(CheckIsSlope()) return true;
+
+        return false;
+
+        bool CheckIsSlope()
+        {
+            Debug.Log("is Slope");
+
+            cachedNormal = hitFo.normal;
+            //Converting angle to dot
+            float minGroundAngle = 25;
+            float angleToRadians = minGroundAngle * Mathf.Deg2Rad;
+            float dotGround = Mathf.Cos(angleToRadians);
+            //Comparing if raycast is considered Ground
+            if (cachedNormal.y < dotGround)
+                return true;
+            else return false;
+        }
     }
 
     public bool CheckIfTouchingGround()
@@ -73,10 +125,9 @@ public class CollisionSenses : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
+        //TO_DO making rays green when detected
+        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position + groundCheckOffset, groundCheckRadius);
-        
-
         Gizmos.DrawRay(smallStepLower.transform.position, transform.TransformDirection(Vector3.forward * rayDistance));
         Gizmos.DrawRay(smallStepUpper.transform.position, transform.TransformDirection(Vector3.forward * rayDistance));
 
@@ -86,5 +137,8 @@ public class CollisionSenses : MonoBehaviour
         Gizmos.DrawRay(smallStepLower.transform.position, transform.TransformDirection(new Vector3(-1.5f, 0, 1) * rayDistance));
         Gizmos.DrawRay(smallStepUpper.transform.position, transform.TransformDirection(new Vector3(-1.5f, 0, 1) * rayDistance));
 
+        Gizmos.color = Color.red;
+        if(CheckIfTouchingGround()) Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position + groundCheckOffset, groundCheckRadius);
     }
 }
